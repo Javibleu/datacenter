294f5c8bb115b267947f1c0e258628cc84dcfcd2
Javier <javibleu@hotmail.com>
2022-09-08 17:06:09 -0400

    feat add nodes nodes-row components

---
+++ src/app/nodes-row/nodes-row.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-nodes-row]',
+  templateUrl: './nodes-row.component.html',
+  styleUrls: ['./nodes-row.component.scss']
+})
+export class NodesRowComponent implements OnInit {
+  @Input() node: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  isDanger(prop: string) {
+    return this.node[prop].used / this.node[prop].available > 0.7;
+  }
+
+}
