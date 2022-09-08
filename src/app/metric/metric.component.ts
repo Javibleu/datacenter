a894ec8146918b785e41b39221762a96ef90e0a0
Javier <javibleu@hotmail.com>
2022-09-08 14:22:21 -0400

    feat add metric

---
+++ src/app/metric/metric.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-metric',
+  templateUrl: './metric.component.html',
+  styleUrls: ['./metric.component.scss']
+})
+export class MetricComponent implements OnInit {
+
+  private _value: number = 0;
+  private _max: number = 100;
+
+  @Input() title: string = '';
+  @Input() description: string = '';
+
+  @Input('used')
+  set value(value: number) {
+    if (isNaN(value)) value = 0;
+    this._value = value;
+  }
+
+  get value(): number { return this._value; }
+
+  @Input('available')
+  set max(max: number) {
+    if (isNaN(max)) max = 100;
+    this._max = max;
+  }
+
+  get max () { return this._max; }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  isDanger() {
+    return this.value / this.max > 0.7;
+  }
+}
